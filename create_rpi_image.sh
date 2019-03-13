6f58a46a0c6a5d9311921f4a12ba0ed3520f080b
Etienne Dublé <etienne.duble@imag.fr>
2019-03-13 09:54:31 +0100

    Auto create draft /etc/walt/Dockerfile

--- create_rpi_image.sh
+++ create_rpi_image.sh
@@ -54,6 +54,35 @@ echo \$led_module > /sys/class/leds/led0/trigger
 EOF
 chmod +x files/bin/blink
 
+cat > files/root/.bashrc << FILE_EOF
+# see https://superuser.com/questions/175799/does-bash-have-a-hook-that-is-run-before-executing-a-command
+preexec_invoke_exec () {
+    [ -n "\$COMP_LINE" ] && return  # do nothing if completing
+    [ "\$BASH_COMMAND" = "\$PROMPT_COMMAND" ] && return # don't cause a preexec for \$PROMPT_COMMAND
+    local this_command=\$(HISTTIMEFORMAT= history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//");
+    echo "RUN \$this_command" >> /etc/walt/Dockerfile
+}
+
+auto_build_dockerfile() {
+    if [ ! -f /.dockerenv ]
+    then    # not in a container
+        return
+    fi
+    if [ ! -f /etc/walt/Dockerfile ]
+    then
+        mkdir -p /etc/walt
+        cat > /etc/walt/Dockerfile << EOF
+# Commands run in walt image shell are saved here.
+# For easier image maintenance, you can retrieve
+# and edit this file to use it as a Dockerfile.
+EOF
+    fi
+    trap 'preexec_invoke_exec' DEBUG
+}
+
+auto_build_dockerfile
+FILE_EOF
+
 cp -p $THIS_DIR/walt-clone-sdcard files/bin/
 cp -p $THIS_DIR/generate-start-uboot.sh \
       $THIS_DIR/config.txt $THIS_DIR/cmdline.txt \
